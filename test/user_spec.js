7d51b5fe68b4878c5a8da99396304a983880774c
aleppington <andy.leppington@quantiv.com>
2014-07-30 17:14:08 +0100

    Added registration and user tests

---
+++ test/user_spec.js
@@ -0,0 +1,40 @@
+var should = require("should");
+var User = require("../models/user");
+
+describe("User", function(){
+   describe("defaults" , function(){
+
+       var user = {};
+
+       before(function (){
+           user = new User({email : "test@quantiv.com"});
+       });
+
+       it("email is test@quantiv.com", function(){
+           user.email.should.equal("test@quantiv.com");
+       });
+
+       it("has an authentication token", function(){
+           user.authenticationToken.should.be.defined;
+       });
+       it("has a pending status", function(){
+           user.status.should.equal("pending");
+       });
+       it("has a created date", function(){
+           user.createdAt.should.be.defined;
+       });
+       it("has a signInCount of 0", function(){
+           user.signInCount.should.equal(0);
+       });
+       it("has a lastLogin", function(){
+           user.lastLoginAt.should.be.defined;
+       });
+       it("has currentLogin", function(){
+           user.currentLoginAt.should.be.defined;
+       });
+//       it("has a sessionToken");
+//       it("has reminderToken");
+//       it("has reminder sent date");
+
+   });
+});
